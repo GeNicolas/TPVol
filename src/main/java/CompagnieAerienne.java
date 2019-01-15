779b416c6764622ed68d5c4045c57f59a9f65d8c
Carla <martosaraujo1svt1@hotmail.fr>
2019-01-15 18:09:47 +0100

    classes vol et compagnieAerienne

---
+++ src/main/java/CompagnieAerienne.java
@@ -0,0 +1,59 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+@Entity 
+@Table(name="compagnie") 
+@SequenceGenerator(name="seqCompagnie", sequenceName="seq_compagnie", allocationSize=1, initialValue=1000) 
+
+public class CompagnieAerienne {
+
+	@Id 
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="seqCompagnie")
+	private Long id;
+	
+	private String nom;
+	
+	@Version
+	private int version;
+	
+	// *** Constructeurs ***
+
+	public CompagnieAerienne(Long id, String nom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+	}
+
+	public CompagnieAerienne() {
+		super();
+	}
+
+	// *** Methodes ***
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/Context.java
@@ -0,0 +1,25 @@
+package util;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Context {
+	private static EntityManagerFactory emf = null;
+
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if (emf == null) {
+			emf = Persistence.createEntityManagerFactory("persistence-unit");
+			System.out.println("-------- Open connection --------");
+			}
+		return emf;
+		}
+	
+	public static void close() {
+		if (emf !=null) {
+			emf.close();
+			emf = null;
+			System.out.println("-------- End of connection --------");
+		}
+	}
+
+}
