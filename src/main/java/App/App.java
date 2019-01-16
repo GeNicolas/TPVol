8cda2446ae547c7b96e9d8c9658e4ddd717dc0e6
GeNicolas <nicolas.gercin@hotmail.fr>
2019-01-16 18:16:48 +0100

    corr in login, passager

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -6,24 +6,35 @@ import java.util.Date;
 
 import Dao.DaoCLientFactory;
 import Dao.DaoClientJpaImpl;
+import Dao.DaoPassager;
+import Dao.DaoPassagerFactory;
 import Dao.DaoReservation;
 import Dao.DaoReservationFactory;
 import Dao.DaoReservationImpl;
 import Model.Client;
 import Model.ClientEI;
+import Model.Passager;
 import Model.Reservation;
+import Util.Context;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		
+		/*
 		Client client = new ClientEI();
 		
 		DaoClientJpaImpl clients = DaoCLientFactory.getInstance();
 		clients.insert(client);
-		System.out.println("blbé");
+		System.out.println("blbé");*/
 		
+		Passager p = new Passager("pierre", "paulJacque");
+		
+		DaoPassager pfactory = DaoPassagerFactory.getPassagerFactory();
+		
+		pfactory.insert(p);
+		
+		Context.getEntityManagerFactory();
 		
 	}
 
