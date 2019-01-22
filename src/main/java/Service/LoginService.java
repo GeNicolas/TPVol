39991147e917f526af3ac8155cc7b0ea3bd7a3cb
GeNicolas <nicolas.gercin@hotmail.fr>
2019-01-22 15:56:59 +0100

    Implementation classe service, Client , Passager, Login

--- src/main/java/Service/LoginService.java
+++ src/main/java/Service/LoginService.java
@@ -1,12 +1,34 @@
 package Service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
+import Model.Client;
+import Model.Login;
+import Repository.ClientRepository;
 import Repository.LoginRepository;
 
 public class LoginService {
 
 	@Autowired
 	private LoginRepository loginRepository;
+	
+	@Autowired
+	private ClientRepository clientRepo;
+	
+	public void deleteLoginWithClient(Long key) {
+		Optional<Login> opt = loginRepository.findById(key);
+		Login login =null;
+		Client c =null;
+		if (opt.isPresent()) {
+			login= opt.get();
+			c =login.getClient();
+			c.setLogin(null);
+			clientRepo.save(c);
+			loginRepository.delete(login);
+		}
+		
+	}
 
 }
